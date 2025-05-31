9e6c953c9cdb1a6af47567b88f4017a9b19e40ba
Bandlapalli Prasanna Kumari <prasuprasanna236@gmail.com>
2025-05-31 10:48:50 +0530

    Add files via upload

---
+++ creating a form in react with the file name App.js.js
@@ -0,0 +1,17 @@
+import React from 'react';
+function App() {
+  return(
+    <>
+    <form>
+      Name:<input type='text' placeholder='enter your name'/><br></br>
+       Password:<input type='password' placeholder='enter your password'/><br></br>
+       Gender:<br></br>
+       Male:<input type='radio' name='a'/><br></br>
+       Female:<input type='radio' name='a'/><br></br>
+       Other:<input type='radio' name='a'/><br></br>
+    </form>
+    </>
+  );
+}
+
+export default App;
